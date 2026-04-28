941094bac79eb837d9d35d5b33bc91d20b1fa841
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-04-28 22:56:02 +0300

    fix: goal model bug

--- goals/models.py
+++ goals/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+import uuid
 
 class Goal(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # userId = models.ForeignKey() # TODO: Connect with user table
     name = models.CharField(max_length=300)
     dueDate = models.DateField()
-    target = models.DecimalField(max_digits=20)
-    current = models.DecimalField(max_digits=20)
+    target = models.DecimalField(max_digits=20, decimal_places=2)
+    current = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
